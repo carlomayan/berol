7d13afbcc5df280da9d307c19dde9b6b669ed856
Carl Omayan <carlomayan@CSAMCebuMacBookPro.local>
2019-08-30 21:58:33 +0800

    refactor name : Gefol

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Berol
+# gefol
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.2.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'berol'`, () => {
+  it(`should have as title 'gefol'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('berol');
+    expect(app.title).toEqual('gefol');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('berol app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('gefol app is running!');
   });
 });
